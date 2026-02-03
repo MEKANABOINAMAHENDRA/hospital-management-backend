abe8edace2034b40c8c078f16ad013ec37094780
MEKANABOINAMAHENDRA <mekanaboinamahendra2@gmail.com>
2026-02-03 21:07:03 +0530

    update corsConfig

--- src/main/java/com/coders/hospitalmanagement/config/CorsConfig.java
+++ src/main/java/com/coders/hospitalmanagement/config/CorsConfig.java
@@ -13,13 +13,22 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+
+        config.setAllowedOriginPatterns(List.of(
+            "http://localhost:5173",
+            "https://hospital-management-frontend-7wqf.onrender.com"
+        ));
+
+        config.setAllowedMethods(List.of(
+            "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+        ));
+
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", config);
 
         return source;
